7b80ebc65b4db549d99be9b672dd7e08b53c8aa9
Radovan.Cervenka@gmail.com <Radovan.Cervenka@gmail.com@9aed9656-3f6c-30d7-10ec-ac811ba5d781>
2014-04-13 14:03:18 +0000

    v0.9 initial version

---
+++ ZgeOuya.cpp
@@ -0,0 +1,398 @@
+/*
+ZgeOuya Library
+Copyright (c) 2014 Radovan Cervenka
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source distribution.
+*/
+
+/**
+  The ZgeOuya Android shared library provides interface
+  to the OUYA In-App Purchasing (IAP) and Storage
+  mechanisms for ZGameEditor.
+
+  Project home
+  http://code.google.com/p/zgeouya/
+*/
+
+// Definitions
+
+#define export extern "C"
+
+//#define LOGGING
+#define LOG_TAG "ZgeOuya"
+
+#ifdef LOGGING
+#	define LOG_DEBUG(str) __android_log_write(ANDROID_LOG_DEBUG, LOG_TAG, str);
+#else
+#	define LOG_DEBUG(str)
+#endif
+
+// returned values
+#define YES 1
+#define NO 0
+#define ERROR -1
+
+// Includes
+
+#include <cstdlib>
+#include <jni.h>
+#include <android/log.h>
+#include <map>
+#include <vector>
+#include <string>
+
+// Types
+
+struct ProductInfo{
+	std::string name;
+	std::string price;
+	std::string description;
+};
+
+struct GamerInfo{
+	std::string username;
+	std::string uuid;
+};
+
+struct Purchase{
+	int result; // = YES / NO / ERROR
+	std::string errorMessage;
+};
+
+// Globals
+
+static JNIEnv *env;
+static jclass zgeOuyaCls;
+static jmethodID isOuyaMethod;
+static jmethodID initPurchasingMethod;
+static jmethodID stopPurchasingMethod;
+static jmethodID requestProductsMethod;
+static jmethodID requestPurchaseMethod;
+static jmethodID requestGamerInfoMethod;
+static jmethodID requestReceiptsMethod;
+static jmethodID putGameDataMethod;
+static jmethodID getGameDataMethod;
+
+std::string gErrorMsg;
+
+int gProductRequestResult; // = YES / NO / ERROR
+std::map<std::string,ProductInfo> gProductInfo;
+
+std::map<std::string,Purchase> gPurchases; // <product id, purchase>
+
+int gReceiptRequestResult; // = YES / NO / ERROR
+std::vector<std::string> gReceipts;
+
+int gGamerRequestResult; // = YES / NO / ERROR
+GamerInfo gGamerInfo;
+
+// Utils
+
+// converts jstring to std::string
+std::string jstring2string(jstring value)
+{
+	const char *n = env->GetStringUTFChars(value, NULL);
+	std::string result = n;
+	env->ReleaseStringUTFChars(value, n);
+
+	return result;
+}
+
+// Functions
+
+// Initialize JNI; used to obtain pointers to Java functions
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved)
+{
+	LOG_DEBUG("JNI OnLoad called");
+
+	int result;
+
+	if(!env){ // if called multiple times, initialize static members just once
+		// get JNI environment
+		result = vm->GetEnv((void**) &env, JNI_VERSION_1_6);
+
+		// set globals JNI variables
+		zgeOuyaCls = env->FindClass("org/zgameeditor/ZgeOuya");
+		isOuyaMethod = env->GetStaticMethodID(zgeOuyaCls, "isOuya", "()Z");
+		initPurchasingMethod = env->GetStaticMethodID(zgeOuyaCls, "initPurchasing", "(Ljava/lang/String;[B)I");
+		stopPurchasingMethod = env->GetStaticMethodID(zgeOuyaCls, "stopPurchasing", "()V");
+		requestProductsMethod = env->GetStaticMethodID(zgeOuyaCls, "requestProducts", "([Ljava/lang/String;)V");
+		requestPurchaseMethod = env->GetStaticMethodID(zgeOuyaCls, "requestPurchase", "(Ljava/lang/String;)V");
+		requestGamerInfoMethod = env->GetStaticMethodID(zgeOuyaCls, "requestGamerInfo", "()V");
+		requestReceiptsMethod = env->GetStaticMethodID(zgeOuyaCls, "requestReceipts", "()V");
+		putGameDataMethod = env->GetStaticMethodID(zgeOuyaCls, "putGameData", "(Ljava/lang/String;Ljava/lang/String;)V");
+		getGameDataMethod = env->GetStaticMethodID(zgeOuyaCls, "getGameData", "(Ljava/lang/String;)Ljava/lang/String;");
+	}
+
+    return JNI_VERSION_1_6;
+}
+
+// Generic
+
+export bool ouya_IsOuya(){
+	LOG_DEBUG("ouya_IsOuya called");
+
+	return (bool)env->CallStaticBooleanMethod(zgeOuyaCls, isOuyaMethod);
+}
+
+// In-App Purchasing (IAP)
+
+export int ouya_InitPurchasing(char* developerId, char *applicationKey, int applicationKeyLen){
+	LOG_DEBUG("ouya_InitPurchasing called");
+
+	jbyteArray key = env->NewByteArray(applicationKeyLen);
+	env->SetByteArrayRegion(key, 0, applicationKeyLen, (jbyte*) applicationKey);
+
+	return env->CallStaticIntMethod(zgeOuyaCls, initPurchasingMethod,
+							env->NewStringUTF(developerId), key);
+}
+
+export void ouya_StopPurchasing(){
+	LOG_DEBUG("ouya_StopPurchasing called");
+
+	env->CallStaticVoidMethod(zgeOuyaCls, stopPurchasingMethod);
+}
+
+export void ouya_RequestProducts(char** productIds, int productNum){
+	LOG_DEBUG("ouya_RequestProducts called");
+
+	jobjectArray productIdArray = env->NewObjectArray(productNum, env->FindClass("java/lang/String"),0);
+	jstring id;
+
+	for(int i=0; i < productNum; ++i){
+		id = env->NewStringUTF(productIds[i]);
+		env->SetObjectArrayElement(productIdArray, i, id);
+	}
+
+	env->CallStaticVoidMethod(zgeOuyaCls, requestProductsMethod, productIdArray);
+
+	gProductInfo.clear();
+	gProductRequestResult = NO;
+}
+
+export int ouya_IsProductRequestDone(const char* &errorMessage){
+	LOG_DEBUG("ouya_IsProductRequestDone called");
+
+	if(gProductRequestResult == ERROR) errorMessage = gErrorMsg.c_str();
+	return gProductRequestResult;
+}
+
+export int ouya_GetProductInfo(char* productId, const char* &name, const char* &price, const char* &description){
+	LOG_DEBUG("ouya_GetProductInfo called");
+
+	if(gProductRequestResult == YES){
+
+		name = gProductInfo[productId].name.c_str();
+		price = gProductInfo[productId].price.c_str();
+		description = gProductInfo[productId].description.c_str();
+	}
+	return gProductRequestResult;
+}
+
+export JNIEXPORT void JNICALL
+Java_org_zgameeditor_ZgeOuya_NativeSetProductInfo(JNIEnv *env, jclass cls,
+	jboolean success, jstring productId, jstring name, jstring price,
+	jstring description, jstring errorMessage){
+
+	LOG_DEBUG("NativeSetProductInfo called");
+
+	if((bool)success){
+
+		ProductInfo newProductInfo;
+
+		newProductInfo.name = jstring2string(name);
+		newProductInfo.price = jstring2string(price);
+		newProductInfo.description = jstring2string(description);
+
+		gProductInfo.insert(std::pair<std::string,ProductInfo>
+							(jstring2string(productId), newProductInfo));
+
+		gProductRequestResult = YES;
+	} else {
+
+		gErrorMsg = jstring2string(errorMessage);
+		gProductRequestResult = ERROR;
+	}
+}
+
+export void ouya_RequestPurchase(char* productId){
+	LOG_DEBUG("ouya_RequestPurchase called");
+
+	Purchase newPurchase;
+	newPurchase.result = NO;
+
+	gPurchases.insert(std::pair<std::string,Purchase>(productId, newPurchase));
+
+	env->CallStaticVoidMethod(zgeOuyaCls, requestPurchaseMethod,
+							env->NewStringUTF(productId));
+}
+
+export int ouya_IsPurchaseRequestDone(char* productId, const char* &errorMessage){
+	LOG_DEBUG("ouya_IsPurchaseRequestDone called");
+
+	if(gPurchases.count(productId) > 0){
+		// product purchase request exists
+		if(gPurchases[productId].result == ERROR)
+			errorMessage = gPurchases[productId].errorMessage.c_str();
+
+		return gPurchases[productId].result;
+
+	} else {
+		// product has not been requested for purchase
+		errorMessage = "Product has not been requested for purchase";
+		return ERROR;
+	}
+}
+
+export void ouya_ClearPurchases(){
+	LOG_DEBUG("ouya_ClearPurchases called");
+
+	gPurchases.clear();
+}
+
+export JNIEXPORT void JNICALL
+Java_org_zgameeditor_ZgeOuya_NativeSetPurchaseInfo(JNIEnv *env, jclass cls,
+	jboolean success, jstring productId, jstring errorMessage){
+
+	LOG_DEBUG("NativeSetPurchaseInfo called");
+
+	const char *id = env->GetStringUTFChars(productId, NULL);
+
+	if((bool)success){
+		gPurchases[id].result = YES;
+	} else {
+		gPurchases[id].errorMessage = jstring2string(errorMessage);
+		gPurchases[id].result = ERROR;
+	}
+
+	env->ReleaseStringUTFChars(productId, id);
+}
+
+export void ouya_RequestReceipts(){
+	LOG_DEBUG("ouya_RequestReceipts called");
+
+	env->CallStaticVoidMethod(zgeOuyaCls, requestReceiptsMethod);
+
+	gReceipts.clear();
+	gReceiptRequestResult = NO;
+}
+
+export int ouya_IsReceiptRequestDone(int &numberOfReceipts, const char* &errorMessage){
+	LOG_DEBUG("ouya_IsReceiptRequestDone called");
+
+	switch(gReceiptRequestResult){
+		case YES:
+			numberOfReceipts = gReceipts.size();
+			break;
+
+		case ERROR:
+			errorMessage = gErrorMsg.c_str();
+	}
+
+	return gReceiptRequestResult;
+}
+
+export int ouya_GetReceiptInfo(int id, const char* &productId){
+	LOG_DEBUG("ouya_GetReceiptInfo called");
+
+	if(id >= 0 && id < gReceipts.size()){
+		productId = gReceipts[id].c_str();
+		return YES;
+	} else
+		return NO;
+}
+
+export JNIEXPORT void JNICALL
+Java_org_zgameeditor_ZgeOuya_NativeSetReceiptInfo(JNIEnv *env, jclass cls,
+	jboolean success, jboolean finished, jstring productId, jstring errorMessage){
+
+	LOG_DEBUG("NativeSetReceiptInfo called");
+
+	if((bool)success){
+		if((bool)finished)
+			// all receipts received
+			gReceiptRequestResult = YES;
+		else
+			// next receipt
+			gReceipts.push_back(jstring2string(productId));
+
+	} else {
+
+		gErrorMsg = jstring2string(errorMessage);
+		gReceiptRequestResult = ERROR;
+	}
+}
+
+export void ouya_RequestGamerInfo(){
+	LOG_DEBUG("ouya_RequestGamerInfo called");
+
+	env->CallStaticVoidMethod(zgeOuyaCls, requestGamerInfoMethod);
+	gGamerRequestResult = NO;
+}
+
+export int ouya_IsGamerInfoRequestDone(const char* &username,
+	const char* &uuid, const char* &errorMessage){
+
+	LOG_DEBUG("ouya_IsGamerInfoRequestDone called");
+
+	switch(gGamerRequestResult){
+		case YES:
+			username = gGamerInfo.username.c_str();
+			uuid = gGamerInfo.uuid.c_str();
+			break;
+
+		case ERROR:
+			errorMessage = gErrorMsg.c_str();
+	}
+
+	return gGamerRequestResult;
+}
+
+export JNIEXPORT void JNICALL
+Java_org_zgameeditor_ZgeOuya_NativeSetGamerInfo(JNIEnv *env, jclass cls,
+	jboolean success, jstring username, jstring uuid, jstring errorMessage){
+
+	LOG_DEBUG("NativeSetGamerInfo called");
+
+	if((bool)success){
+		gGamerInfo.username = jstring2string(username);
+		gGamerInfo.uuid = jstring2string(uuid);
+		gGamerRequestResult = YES;
+	} else {
+		gErrorMsg = jstring2string(errorMessage);
+		gGamerRequestResult = ERROR;
+	}
+}
+
+// Storage
+
+export void ouya_PutGameData(char* name, char* value){
+	LOG_DEBUG("ouya_PutGameData called");
+
+	env->CallStaticVoidMethod(zgeOuyaCls, putGameDataMethod,
+			env->NewStringUTF(name), env->NewStringUTF(value));
+}
+
+export const char* ouya_GetGameData(char* name){
+	LOG_DEBUG("ouya_GetGameData called");
+
+	jstring data = (jstring) env->CallStaticObjectMethod(zgeOuyaCls, getGameDataMethod,
+										env->NewStringUTF(name));
+	return data == NULL ? "" : env->GetStringUTFChars(data, NULL);
+}
---
+++ java/ZgeOuya.java
@@ -0,0 +1,298 @@
+/*
+ZgeOuya Library
+Copyright (c) 2014 Radovan Cervenka
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source distribution.
+*/
+
+package org.zgameeditor;
+
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.json.JSONObject;
+
+import tv.ouya.console.api.CancelIgnoringOuyaResponseListener;
+import tv.ouya.console.api.GamerInfo;
+import tv.ouya.console.api.OuyaEncryptionHelper;
+import tv.ouya.console.api.OuyaFacade;
+import tv.ouya.console.api.OuyaResponseListener;
+import tv.ouya.console.api.Product;
+import tv.ouya.console.api.Purchasable;
+import tv.ouya.console.api.Receipt;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+public class ZgeOuya {
+  
+  private static final String LOG_TAG = "ZgeOuya";
+  private static final String CANCEL_PURCHASE_MESSAGE = "User cancelled purchase";
+  private static final int YES = 1;
+  private static final int NO = 0;
+  private static final int ERROR = -1;
+  
+  private static final OuyaFacade facade = OuyaFacade.getInstance();
+  private static PublicKey mPublicKey;
+
+  static {
+    System.loadLibrary("ZgeOuya");
+  }
+  
+  private static native void NativeSetProductInfo(boolean success,
+      String productId, String name, String price, String description,
+      String errorMessage);
+
+  private static native void NativeSetGamerInfo(boolean success,
+      String username, String uuid, String errorMessage);
+  
+  private static native void NativeSetPurchaseInfo(boolean success,
+      String productId, String errorMessage);
+
+  private static native void NativeSetReceiptInfo(boolean success,
+      boolean finished, String productId, String errorMessage);
+
+  // Generic
+  
+  public static boolean isOuya() {
+    return facade.isRunningOnOUYAHardware();
+  }
+
+  // In-App Purchasing
+
+  public static synchronized int initPurchasing(String developerId, byte[] applicationKey) {
+    facade.init(ZgeActivity.zgeActivity, developerId);
+ 
+    // create public key
+    try {
+      X509EncodedKeySpec keySpec = new X509EncodedKeySpec(applicationKey);
+      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+      mPublicKey = keyFactory.generatePublic(keySpec);
+    } catch (Exception e) {
+      Log.e(LOG_TAG, "Unable to create encryption key", e);
+      return ERROR;
+    }
+    return YES;
+  }
+
+  public static void stopPurchasing() {
+    facade.shutdown();
+  }
+  
+  // Product info
+  
+  // product info request callback
+  private static final OuyaResponseListener<ArrayList<Product>> productListListener =
+    new CancelIgnoringOuyaResponseListener<ArrayList<Product>>() {
+
+    @Override
+    public void onSuccess(ArrayList<Product> products) {
+      for(Product p : products) {
+        NativeSetProductInfo(true, p.getIdentifier(), p.getName(),
+            p.getLocalPrice() + " " + p.getCurrencyCode(), p.getDescription(), null);
+      }
+    }
+
+    @Override
+    public void onFailure(int errorCode, String errorMessage, Bundle errorBundle) {
+      NativeSetProductInfo(false, null, null, null, null, errorMessage);
+    }
+  };
+
+  public static void requestProducts(String[] productIds) {
+    ArrayList<Purchasable> productList = new ArrayList<Purchasable>();
+    for(String productId: Arrays.asList(productIds))
+      productList.add(new Purchasable(productId));
+    
+    facade.requestProductList(productList, productListListener);
+  }
+
+  // Making purchases
+
+  private static final Map<String, String> mOutstandingPurchaseRequests = new HashMap<String, String>();
+
+  // purchase request callback
+  private static class PurchaseListener implements OuyaResponseListener<String> {
+
+    private String mProductId;
+
+    PurchaseListener(final String productId) {
+      mProductId = productId;
+    }
+
+    @Override
+    public void onSuccess(String result) {
+
+      try {
+        String id;
+        OuyaEncryptionHelper helper = new OuyaEncryptionHelper();
+
+        JSONObject response = new JSONObject(result);
+        id = helper.decryptPurchaseResponse(response, mPublicKey);
+        String storedProductId;
+        synchronized (mOutstandingPurchaseRequests) {
+          storedProductId = mOutstandingPurchaseRequests.remove(id);
+        }
+
+        if(storedProductId == null) {
+          NativeSetPurchaseInfo(false, mProductId,
+              "No purchase outstanding for the given purchase request");
+          return;
+        }
+
+        NativeSetPurchaseInfo(true, storedProductId, null);
+
+      } catch (Exception e) {
+        NativeSetPurchaseInfo(false, mProductId, e.getMessage());
+      }
+    }
+
+    @Override
+    public void onFailure(int errorCode, String errorMessage, Bundle optionalData) {
+      NativeSetPurchaseInfo(false, mProductId, errorMessage);
+    }
+
+    @Override
+    public void onCancel() {
+      NativeSetPurchaseInfo(false, mProductId, CANCEL_PURCHASE_MESSAGE);
+    }
+  }
+
+  @SuppressLint("TrulyRandom")
+  public static void requestPurchase(String productId) {
+
+    try {
+      SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+      String uniqueId = Long.toHexString(sr.nextLong());
+  
+      JSONObject purchaseRequest = new JSONObject();
+      purchaseRequest.put("uuid", uniqueId);
+      purchaseRequest.put("identifier", productId);
+      String purchaseRequestJson = purchaseRequest.toString();
+  
+      byte[] keyBytes = new byte[16];
+      sr.nextBytes(keyBytes);
+      SecretKey key = new SecretKeySpec(keyBytes, "AES");
+  
+      byte[] ivBytes = new byte[16];
+      sr.nextBytes(ivBytes);
+      IvParameterSpec iv = new IvParameterSpec(ivBytes);
+  
+      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
+      cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+      byte[] payload = cipher.doFinal(purchaseRequestJson.getBytes("UTF-8"));
+  
+      cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding", "BC");
+      cipher.init(Cipher.ENCRYPT_MODE, mPublicKey);
+      byte[] encryptedKey = cipher.doFinal(keyBytes);
+  
+      Purchasable purchasable =
+          new Purchasable(
+              productId,
+              Base64.encodeToString(encryptedKey, Base64.NO_WRAP),
+              Base64.encodeToString(ivBytes, Base64.NO_WRAP),
+              Base64.encodeToString(payload, Base64.NO_WRAP) );
+  
+      synchronized (mOutstandingPurchaseRequests) {
+        mOutstandingPurchaseRequests.put(uniqueId, productId);
+      }
+      
+      facade.requestPurchase(purchasable, new PurchaseListener(productId));
+    } catch (Exception e) {
+      Log.e(LOG_TAG, e.getMessage(), e);
+    }
+  }
+  
+  // Purchase receipt
+
+  // receipt info request callback
+  private static final CancelIgnoringOuyaResponseListener<String> receiptListListener =
+      new CancelIgnoringOuyaResponseListener<String>() {
+          @Override
+          public void onSuccess(String receiptResponse) {
+              OuyaEncryptionHelper helper = new OuyaEncryptionHelper();
+              List<Receipt> receipts = null;
+              try {
+                  JSONObject response = new JSONObject(receiptResponse);
+                  receipts = helper.decryptReceiptResponse(response, mPublicKey);
+              } catch (Exception e) {
+                  NativeSetReceiptInfo(false, false, null, e.getMessage());
+                  return;
+              }
+
+              for (Receipt r : receipts) {
+                NativeSetReceiptInfo(true, false, r.getIdentifier(), null);  
+              }
+              NativeSetReceiptInfo(true, true, null, null);
+          }
+
+          @Override
+          public void onFailure(int errorCode, String errorMessage, Bundle errorBundle) {
+            NativeSetReceiptInfo(false, false, null, errorMessage);
+          }
+      };
+
+  public static void requestReceipts() {
+    facade.requestReceipts(receiptListListener);
+  }
+ 
+  // Gamer info
+
+  // gamer info request callback
+  private static final CancelIgnoringOuyaResponseListener<GamerInfo> gamerInfoListener =
+      new CancelIgnoringOuyaResponseListener<GamerInfo>() {
+
+    @Override
+    public void onSuccess(GamerInfo result) {
+      NativeSetGamerInfo(true, result.getUsername(), result.getUuid(), null);        
+    }
+  
+    @Override
+    public void onFailure(int errorCode, String errorMessage, Bundle errorBundle) {
+      NativeSetGamerInfo(false, null, null, errorMessage);
+    }
+  };
+
+  public static void requestGamerInfo() {
+    facade.requestGamerInfo(gamerInfoListener);
+  }
+  
+  // Storage
+  
+  public static void putGameData(String name, String value) {
+    facade.putGameData(name, value);
+  }
+  
+  public static String getGameData(String name) {
+    return facade.getGameData(name);
+  }
+}
