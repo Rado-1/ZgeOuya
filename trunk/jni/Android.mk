94e30cc267f8416f58592723dec6bc434fbfb2f8
Radovan.Cervenka@gmail.com <Radovan.Cervenka@gmail.com@9aed9656-3f6c-30d7-10ec-ac811ba5d781>
2014-04-13 14:03:18 +0000

    v0.9 initial version

---
+++ trunk/jni/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)/..
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ZgeOuya
+LOCAL_SRC_FILES := ZgeOuya.cpp
+LOCAL_C_INCLUDES := $(NDK_ROOT)/sources/cxx-stl/stlport/stlport
+
+#comment if logging is edisabled
+#LOCAL_LDLIBS += -llog
+
+include $(BUILD_SHARED_LIBRARY)
